98ea9339e7c1ae2976c560be128cc5f4a79a8f15
Albert Guo <albertguotrs@hotmail.com>
2019-10-04 16:08:41 -0500

    stability improvement

--- DNS Proxy/part1.py
+++ DNS Proxy/part1.py
@@ -9,23 +9,25 @@ CLIENT_ADDRESS = ('127.0.0.1', 53)
 def server():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_receive:
         s_receive.bind(CLIENT_ADDRESS)
-        while True:
+        while True: # keep listening
             query, address = s_receive.recvfrom(4096)
             if query:
                 print('query:', query)
                 print('query address:', address)
-                
                 print('\nforwarding query to Google DNS server...')
-                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_forward:
-                    s_forward.sendto(query, DNS_SERVER_ADDRESS)
-                    response, response_address = s_forward.recvfrom(4096)
-                    if response:
-                        print('response:', response)
-                        print('response address:', response_address)
 
-                        s_forward.sendto(response, CLIENT_ADDRESS)
-                        print('response send back to client:', s_receive.recv(4096))
-                        break
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_forward:
+                    while True: # keep sending queries until receiving a response
+                        s_forward.sendto(query, DNS_SERVER_ADDRESS)
+                        response, response_address = s_forward.recvfrom(4096)
+                        if response:
+                            print('response:', response)
+                            print('response address:', response_address)
+
+                            s_forward.sendto(response, CLIENT_ADDRESS)
+                            print('response send back to client:', s_receive.recv(4096))
+                            break
+                break
 
 
 if __name__ == '__main__':

fb795960a55e93f82db094d0b952701a5cf49d5c
Albert Guo <albertguotrs@hotmail.com>
2019-10-06 22:13:25 -0500

    updates

--- DNS Proxy/part2.py
+++ DNS Proxy/part2.py
@@ -14,25 +14,36 @@ def parse_udp(packet):
     (source_port, dest_port, data_length, checksum) = struct.unpack("!HHHH", header)
     return source_port, dest_port, data_length, checksum, data
 
+def extract_string(bytes):
+    message = []
+    for i in bytes:
+        if i >= 3:
+            if i < 10:
+                message.append('.')
+            else:
+                message.append(chr(i))
+    return ''.join(message[1:])
+
 def dns_proxy_over_http():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_receive:
         s_receive.bind(CLIENT_ADDRESS)
         while True: # keep listening
             query, address = s_receive.recvfrom(4096)
             if query:
-                print('query:', query)
-                print('query address:', address)
+                print('query:           ', query)
+                print('query address:   ', address)
 
-                print(len(query))
                 source_port, dest_port, data_length, checksum, data = parse_udp(query)
-                print(source_port, dest_port, data_length, checksum, data)
+                print('source port:     ', source_port)
+                print('destination port:', dest_port)
+                print('data length:     ', data_length)
+                print('checksum:        ', checksum)
 
+                print('data:            ', data)
+                print('data decoded:    ', extract_string(data))
 
-                parameters = {
-                    'name': 'baidu.com'
-                }
+                parameters = {'name': extract_string(data)}
                 response = requests.get('https://dns.google/resolve', params=parameters)
-                print(response.status_code)
                 print(response.json())
 
                 break

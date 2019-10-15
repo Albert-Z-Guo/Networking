489d48d0c03ef1814dcc477c12e1a0ee73353166
Albert Guo <albertguotrs@hotmail.com>
2019-10-15 16:07:17 -0500

    updates

--- DNS Proxy/part1.py
+++ DNS Proxy/part1.py
@@ -2,32 +2,27 @@ import socket
 
 
 # IPv4 addresses with privileged port
-DNS_SERVER_ADDRESS = ('8.8.8.8', 53) # Google public DNS servers
+DNS_SERVER_ADDRESS = ('8.8.8.8', 53)  # Google public DNS servers
 CLIENT_ADDRESS = ('127.0.0.1', 53)
 
 
 def dns_proxy():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_receive:
-        s_receive.bind(CLIENT_ADDRESS)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socket_client:
+        socket_client.bind(CLIENT_ADDRESS)
         while True: # keep receiving
-            query, address = s_receive.recvfrom(4096)
+            query, address = socket_client.recvfrom(4096)
             if query:
-                print('query:', query)
+                print('\nquery:', query)
                 print('query address:', address)
-                print('\nforwarding query to Google DNS server...')
 
-                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_forward:
-                    while True: # keep sending and receiving
-                        s_forward.sendto(query, DNS_SERVER_ADDRESS)
-                        response, response_address = s_forward.recvfrom(4096)
-                        if response:
-                            print('response:', response)
-                            print('response address:', response_address)
-
-                            s_forward.sendto(response, CLIENT_ADDRESS)
-                            print('response send back to client:', s_receive.recv(4096))
-                            break
-                break
+                # forward query to Google DNS server
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socket_server:
+                    socket_server.sendto(query, DNS_SERVER_ADDRESS)
+                    response, response_address = socket_server.recvfrom(4096)
+                    if response:
+                        print('response:', response)
+                        print('response address:', response_address)
+                        socket_client.sendto(response, address)
 
 
 if __name__ == '__main__':
--- DNS Proxy/part2.py
+++ DNS Proxy/part2.py
@@ -1,26 +1,28 @@
 import socket
-import requests
 
+import requests
 from dnslib import *
 
 # IPv4 addresses with privileged port
 CLIENT_ADDRESS = ('127.0.0.1', 53)
 
 def dns_proxy_over_http():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_receive:
-        s_receive.bind(CLIENT_ADDRESS)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socket_client:
+        socket_client.bind(CLIENT_ADDRESS)
         while True: # keep listening
-            query, address = s_receive.recvfrom(4096)
+            query, address = socket_client.recvfrom(4096)
             if query:
-                # parse query
-                message = DNSRecord.parse(query)
-                question = message.questions[0]
-                query_type = message.questions[0].qtype
-                query_class = message.questions[0].qclass
-                query_header = message.header
+                # parse DNS query message
+                message_query = DNSRecord.parse(query)
+                question = message_query.questions[0].qname
+                query_type = message_query.questions[0].qtype
+                query_class = message_query.questions[0].qclass # Class field is 1 for Internet data.
+                query_header = message_query.header
 
-                # print('message:', message)
-                # print('question:', message.questions)
+                # print('query')
+                # print('question:', question)
+                # print('type:', query_type)
+                # print('class:', query_class)
                 # print('QR:', query_header.qr)
                 # print('AA:', query_header.aa)
                 # print('TC:', query_header.tc)
@@ -31,57 +33,50 @@ def dns_proxy_over_http():
 
                 # https://developers.google.com/speed/public-dns/docs/doh/json
                 # send query to Google DNS server
-                parameters = {'name': str(question.qname), 'type': query_type}
-                google_dns_response = requests.get('https://dns.google/resolve', params=parameters).json()
-                print(google_dns_response)
+                parameters = {'name': str(question), 'type': query_type}
+                response = requests.get('https://dns.google/resolve', params=parameters).json()
+                print(response)
 
-                # parse response from Google DNS server
-                TC = google_dns_response['TC']
-                RD = google_dns_response['RD']
-                RA = google_dns_response['RA']
-                AD = google_dns_response['AD']
-                CD = google_dns_response['CD']
-                query_type = google_dns_response['Question'][0]['type'] # type 1 means type A
-                name = google_dns_response['Answer'][0]['name']
-                TTL = google_dns_response['Answer'][0]['TTL']
-                data = google_dns_response['Answer'][0]['data']
-
-                # construct response
+                # construct DNS response message
                 query_header.set_qr(1) # set QR = 1 for response
-                query_header.set_tc(TC)
-                query_header.set_rd(RD)
-                query_header.set_ra(RA)
-                query_header.set_ad(AD)
-                query_header.set_cd(CD)
-                rdata = A(data)
-                if query_type == 5:
-                    rdata = CNAME(data)
+                query_header.set_tc(response['TC'])
+                query_header.set_rd(response['RD']) # always true for Google Public DNS
+                query_header.set_ra(response['RA'] ) # always true for Google Public DNS
+                query_header.set_ad(response['AD'])
+                query_header.set_cd(response['CD'])
 
-                response = DNSRecord(query_header, q=question, a=RR(rname=name, rtype=query_type, rclass=query_class, ttl=TTL, rdata=rdata))
+                message_response = DNSRecord(query_header)
 
-                # print('RA:', RA)
-                # print('type:', query_type)
-                # print('name:', name)
-                # print('TTL:', TTL)
-                # print('data:', data)
-                # print('QR:', query_header.qr)
-                # print('AA:', query_header.aa)
-                # print('TC:', query_header.tc)
-                # print('RD:', query_header.rd)
-                # print('RA:', query_header.ra)
-                # print('AD:', query_header.ad)
-                # print('CD:', query_header.cd)
-                # print(response)
+                # in case of multiple questions and answers
+                for question in response['Question']:
+                    message_response.add_question(DNSQuestion(qname=question['name'], qtype=question['type']))
+                for answer in response['Answer']:
+                    rdata = CNAME(answer['data']) if answer['type'] == 5 else A(answer['data']) # type = 1 means type A; type = 5 means type CNAME
+                    message_response.add_answer(RR(rname=answer['name'], rtype=answer['type'], rclass=query_class, ttl=answer['TTL'], rdata=rdata))
+
+                # print('response:')
+                # print('name:', message_response.a.rname)
+                # print('type:', message_response.a.rtype)
+                # print('TTL:', message_response.a.ttl) #  TTL field is the number of seconds for which the RR can be cached.
+                # print('data:', message_response.a.rdata)
+                # print('QR:', query_header.qr) # Query (0) Response (1)
+                # print('AA:', query_header.aa) # Authoritative Answer
+                # print('TC:', query_header.tc) # Truncated Answer
+                # print('RD:', query_header.rd) # Recursion Desired
+                # print('RA:', query_header.ra) # Recursion Available
+                # print('AD:', query_header.ad) # Authentic Data
+                # print('CD:', query_header.cd) # Checking Disabled
 
                 # send the response back to client
-                s_receive.sendto(response.pack(), CLIENT_ADDRESS)
-                print('response sent back to client:', s_receive.recv(4096))
-                break
+                socket_client.sendto(message_response.pack(), address)
+                print('response sent back to client:', message_response.pack())
+
+                # example response from part1.py
+                # b'\xd3\x0b\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\xe3\x00\x04\xd8:\xc0\xee'
 
                 # example response from part2.py
-                # b'\xa5V\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x9b\x00\x04\xac\xd9\x05\x0e'
-                # example response from part1.py
-                # b'\xd4!\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\xbc\x00\x04\xac\xd9\x05\x0e'
+                # b'\x8f\xc5\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x01 \x00\x04\xd8:\xc0\xee'
+
 
 if __name__ == '__main__':
     dns_proxy_over_http()

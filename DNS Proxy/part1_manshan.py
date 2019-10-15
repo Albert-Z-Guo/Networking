7ec215ded45aff8ec631684aa745e294e3ed2c41
Albert Guo <albertguotrs@hotmail.com>
2019-10-15 16:08:46 -0500

    formatting

--- DNS Proxy/part1_manshan.py
+++ DNS Proxy/part1_manshan.py
@@ -1,27 +1,23 @@
 import sys
 import socket
-import dns.resolver
+
 
 def server():
     # Create a TCP/IP socket
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            res=''
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        res = ''
         # Bind the socket to the port
-            server_address = ('127.0.0.1', 53)
-            sock.bind(server_address)
-            while True:
-                data, address = sock.recvfrom(4096)
-                if data:
-                    sock1=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    DNS_server=('8.8.8.8',53)
-                    sock1.sendto(data,DNS_server)
-                    res, server = sock1.recvfrom(4096)
-                    sock.sendto(res,address)
-                    sock1.close()
-              
+        server_address = ('127.0.0.1', 53)
+        sock.bind(server_address)
+        while True:
+            data, address = sock.recvfrom(4096)
+            if data:
+                sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                DNS_server = ('8.8.8.8', 53)
+                sock1.sendto(data, DNS_server)
+                res, server = sock1.recvfrom(4096)
+                sock.sendto(res, address)
+                sock1.close()
+
+
 server()
-
-
-
-
-

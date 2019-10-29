86368d24f664507b9b43bd9563af36da4b2e036e
Albert Guo <albertguotrs@hotmail.com>
2019-10-29 14:37:57 -0500

    basic echo server function

--- 02 Partial Reliable Transport Protocol/wildcat_receiver.py
+++ 02 Partial Reliable Transport Protocol/wildcat_receiver.py
@@ -1,3 +1,5 @@
+import time
+
 import common
 import threading
 
@@ -10,16 +12,30 @@ class wildcat_receiver(threading.Thread):
         self.my_logger = my_logger
         self.die = False
         # add as needed
+        self.window_starting_position = 1
 
     def receive(self, packet_byte_array):
-        # TODO: your implementation comes here
-        pass
+        print(packet_byte_array)
+        seq_num = packet_byte_array[:2]
+        data = packet_byte_array[2:-2]
+        checksum = packet_byte_array[-2:]
+        print(seq_num, data, checksum)
+
+        if sum(data).to_bytes(2, byteorder='big') == checksum:
+            ack = self.window_starting_position.to_bytes(2, byteorder='big') + (1).to_bytes(1, byteorder='big') + packet_byte_array[-2:]
+            print('no corruption')
+        else:
+            ack = self.window_starting_position.to_bytes(2, byteorder='big') + (0).to_bytes(1, byteorder='big') + packet_byte_array[-2:]
+            print('data corrupted')
+
+        self.my_tunnel.magic_send(ack)
+        self.my_logger.commit(packet_byte_array)
+
 
     def run(self):
         while not self.die:
-            # TODO: your implementation comes here
-            pass
-            
+            time.sleep(2)
+
     def join(self):
         self.die = True
         super().join()
--- 02 Partial Reliable Transport Protocol/wildcat_sender.py
+++ 02 Partial Reliable Transport Protocol/wildcat_sender.py
@@ -1,3 +1,5 @@
+import time
+
 import common
 import threading
 
@@ -10,16 +12,34 @@ class wildcat_sender(threading.Thread):
         self.my_logger = my_logger
         self.die = False
         # add as needed
+        self.base = 1
+        self.nextseqnum = 1
+        self.timer = threading.Timer(5.0, self.timeout)
+
 
     def new_packet(self, packet_byte_array):
+        seq_num = self.nextseqnum.to_bytes(2, byteorder='big')
+        checksum = sum(packet_byte_array).to_bytes(2, byteorder='big')
+        print('seq_num:', seq_num)
+        print('checksum:', checksum)
+        packet_byte_array[0:0] = seq_num # insert sequence number at the beginning
+        packet_byte_array += checksum # append checksum
         self.my_tunnel.magic_send(packet_byte_array)
 
+
     def receive(self, packet_byte_array):
-        self.my_tunnel.recv_queue.put(packet_byte_array)
+        print(packet_byte_array)
+
 
     def run(self):
         while not self.die:
-            pass
+            time.sleep(2)
+            # if self.nextseqnum < self.base + self.window_size:
+            #     print('sending...')
+            #     self.my_tunnel.magic_send(self.new_packet(packet_byte_array))
+            #     time.sleep(2)
+            #     break
+
 
     def join(self):
         self.die = True

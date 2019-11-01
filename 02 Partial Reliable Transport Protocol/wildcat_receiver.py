268ea5d771a31b9d513ee630ef5adcaba8b014ce
Albert Guo <albertguotrs@hotmail.com>
2019-11-01 17:50:43 -0500

    clean and optimize code

--- 02 Partial Reliable Transport Protocol/wildcat_receiver.py
+++ 02 Partial Reliable Transport Protocol/wildcat_receiver.py
@@ -23,30 +23,29 @@ class wildcat_receiver(threading.Thread):
         if sum(packet_byte_array[:-2]) == checksum:
             ack_payload = packet_seq_num.to_bytes(2, byteorder='big') + (1).to_bytes(1, byteorder='big')
             ack = ack_payload + sum(ack_payload).to_bytes(2, byteorder='big')
-            
+
             if self.base <= packet_seq_num < self.base + self.window_size:
                 self.my_tunnel.magic_send(bytearray(ack))
-                print('ack_seq_num sent    :', packet_seq_num)
+                print('ack_seq_num sent:', packet_seq_num)
                 # if the packet's sequence number is the base, this packet and 
                 # any previously buffered and consecutively numbered packets are delivered
                 if packet_seq_num == self.base:
                     self.my_logger.commit(bytearray(packet_byte_array))
                     self.base += 1
-                    while (self.base in self.buffer.keys()):
+                    while self.base in self.buffer.keys():
                         self.my_logger.commit(bytearray(self.buffer[self.base]))
                         del self.buffer[self.base]
                         self.base += 1
                 # buffer packet
                 else:
                     self.buffer[packet_seq_num] = packet_byte_array
+            
             # send duplicated ack
             if packet_seq_num < self.base:
                 self.my_tunnel.magic_send(bytearray(ack))
-
         # if data is corrupted
         else:
-            ack_payload = (0).to_bytes(1, byteorder='big')
-            print('received packet corrupted:', packet_seq_num)
+            print('received packet corrupted')
 
     def run(self):
         while not self.die:
--- 02 Partial Reliable Transport Protocol/wildcat_sender.py
+++ 02 Partial Reliable Transport Protocol/wildcat_sender.py
@@ -1,6 +1,4 @@
 import time
-import queue
-import collections 
 
 import common
 import threading
@@ -14,35 +12,31 @@ class wildcat_sender(threading.Thread):
         self.my_logger = my_logger
         self.die = False
         # add as needed
-        self.buffer = []
+        self.buffer = {}
         self.packet_seq_num = 1
         self.next_packet_seq_num = 1
         self.base = 1
-        self.start_time = time.time()
         self.packet_times = {}
         self.acks = set()
 
     def new_packet(self, packet_byte_array):
         packet_seq_num = self.packet_seq_num.to_bytes(2, byteorder='big')
-        # packet_byte_array = packet_seq_num + packet_byte_array 
-        packet_byte_array[0:0] = packet_seq_num # insert sequence number at the beginning
+        packet_byte_array[0:0] = packet_seq_num # insert sequence number in bytearray
         checksum = sum(packet_byte_array).to_bytes(2, byteorder='big')
         packet_byte_array += checksum # append checksum
-        self.buffer.append(packet_byte_array)
+        self.buffer[self.packet_seq_num] = packet_byte_array 
         self.packet_seq_num += 1
 
     def check_time(self):
         for packet_num in range(self.base, self.base+self.window_size):
             if packet_num in self.packet_times and time.time() - self.packet_times[packet_num] > 0.5: # if timeout
-                print('timed out:', time.time() - self.packet_times[packet_num])
-                self.my_tunnel.magic_send(bytearray(self.buffer[packet_num-1])) # resend packet
+                self.my_tunnel.magic_send(bytearray(self.buffer[packet_num])) # resend packet
                 self.packet_times[packet_num] = time.time() # start timer
-                print('resending packet:', int.from_bytes(self.buffer[packet_num-1][:2], byteorder='big'))
+                print('resending packet:', int.from_bytes(self.buffer[packet_num][:2], byteorder='big'))
 
     def receive(self, packet_byte_array):
         ack_seq_num = int.from_bytes(packet_byte_array[:2], byteorder='big') 
         checksum = int.from_bytes(packet_byte_array[-2:], byteorder='big')
-
         # if ack is not corrupted
         if sum(packet_byte_array[:-2]) == checksum:
             if self.base <= ack_seq_num < self.base + self.window_size: # if packet is in the window
@@ -51,13 +45,13 @@ class wildcat_sender(threading.Thread):
                 if ack_seq_num == self.base:
                     self.base += 1
                     # move base forward to the unacknowledged packet with the smallest sequence number
-                    while (self.base in self.acks):
+                    while self.base in self.acks:
                         self.base += 1
                         print('base now:', self.base)
         # if ack is corrupted
         else:
             print('ack corrupted')
-            self.my_tunnel.magic_send(bytearray(self.buffer[self.base-1])) # resend base packet
+            self.my_tunnel.magic_send(bytearray(self.buffer[self.base])) # resend base packet
 
     def run(self):
         while not self.die:
@@ -66,7 +60,7 @@ class wildcat_sender(threading.Thread):
                 # send data if the next available sequence number is within window
                 if self.next_packet_seq_num < self.base + self.window_size:    
                     print('sending packet...\tbase:', self.base, '\tnext_packet_seq_num:', self.next_packet_seq_num)
-                    data = self.buffer[self.next_packet_seq_num-1]
+                    data = self.buffer[self.next_packet_seq_num]
                     self.my_tunnel.magic_send(bytearray(data))
                     self.packet_times[self.next_packet_seq_num] = time.time() # time individual packet
                     self.next_packet_seq_num += 1
